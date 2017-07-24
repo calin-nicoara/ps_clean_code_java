2816ec9ba0a11fe43090d3a1661a22bc53f03c06
calin_nicoara <calin.nicoara@esolutions.ro>
2017-07-24 12:27:19 +0300

    First commit

---
+++ src/main/java/ro/nci/cleancodejava/CleanCodeJavaApplication.java
@@ -0,0 +1,12 @@
+package ro.nci.cleancodejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeJavaApplication.class, args);
+	}
+}
